b6c1bb47e702a13f4d45873479b30e1075e9aff7
Ashwani <ashwanix2749@gmail.com>
2023-07-29 14:31:48 +0530

    intial

---
+++ backend/index.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const multer = require('multer');
+const mongoose = require('mongoose');
+// const path = require('path');
+const cors = require('cors');
+// const router = express.Router();
+const app = express();
+const PORT = 3001;
+
+// app.use(cors());
+app.use(cors({
+  origin:'http://localhost:3000',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}))
+
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://ashwanix2749:brNEUnJl9b0N9LMF@cluster1.wsuyb84.mongodb.net/upload', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Create a schema and model for the uploaded image
+const imageSchema = new mongoose.Schema({
+  imageName: String,
+  imagePath: String,
+});
+
+const Image = mongoose.model('Image', imageSchema);
+
+// Configure Multer for image uploads
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now()+"_"+file.originalname);
+  },
+});
+const upload = multer({ storage: storage });
+
+app.post('/upload', upload.single('image'), async (req, res) => {
+  const newImage = new Image({
+    imageName: req.file.filename,
+    imagePath: req.file.path,
+  });
+
+  try {
+    const image = await newImage.save();
+    res.json({ message: 'Image uploaded successfully',"image": image });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: 'Failed to save image to the database' });
+  }
+});
+app.get("/fetchall", async (req, res) => {
+  try {
+    // console.log("index ki 58 line");
+    const img = await Image.find();
+    // console.log(img);
+    res.json(img);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+
+// Route to serve uploaded images
+app.use('/uploads', express.static('uploads'));
+app.get("/", (req, res) => {
+  return res.json( "hello i am 3001" );
+});
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
+// module.exports = router;
---
+++ frontend/src/App.js
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from 'react';
+
+const App = () => {
+  const [image, setImage] = useState();
+  const [imagesList, setImagesList] = useState([]);
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    formData.append("image", image);
+
+    try {
+      const response = await fetch('http://localhost:3001/upload', {
+        method: 'POST',
+        body: formData,
+      });
+      const data = await response.json();
+      setImage(data.image);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const fetchall = async () => {
+    try {
+      const response = await fetch("http://localhost:3001/fetchall", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const json = await response.json();
+      // console.log(json);
+      setImagesList(json); 
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchall();
+    // eslint-disable-next-line
+  }, []);
+
+  return (
+    <>
+      <div>
+        <h1>Image Uploader</h1>
+        <form onSubmit={handleFormSubmit} action="/upload" method="post" encType="multipart/form-data">
+          <div style={{ width: '300px', height: '300px', border: '1px solid #ccc' }}>
+            {image ? (
+              <img src={`http://localhost:3001/${image.imagePath}`} alt="Uploaded" style={{ width: '100%', height: '100%', objectFit: 'contain' }} />
+            ) : (
+              'Drag and drop an image here'
+            )}
+          </div>
+
+          <input type="file" name="image" />
+          <button type="submit">Upload</button>
+        </form>
+      </div>
+
+      <h2 className="my-3">IMAGES</h2>
+      <div className="row my-3">
+        {imagesList.map((img) => (
+          <img key={img._id} src={`http://localhost:3001/${img.imagePath}`} alt="Uploaded" style={{ width: '200px', height: '200px', objectFit: 'cover', margin: '10px' }} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default App;
